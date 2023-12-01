cd6d50cf8a143d89224d1748f13090a66c4a641d
Vector-ops <sumith2827@gmail.com>
2023-12-01 23:01:39 +0530

    feat: add user registration functionality with validation and error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,17 @@
+import argon2 from "argon2";
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import prisma from "../config/prisma";
 import { logger } from "../utils/logger";
 import { registerSchema } from "../utils/validation";
+
+declare module "express-session" {
+	export interface SessionData {
+		passport: {
+			user: string;
+		};
+	}
+}
 
 /**
  * @desc    Register a new user
@@ -19,12 +28,38 @@ export const registerUser = async (
 		const { name, email, username, password } =
 			await registerSchema.validateAsync(req.body);
 
+		const emailExists = await prisma.user.findFirst({
+			where: {
+				email,
+			},
+		});
+		const usernameExists = await prisma.user.findFirst({
+			where: {
+				username,
+			},
+		});
+
+		if (emailExists) {
+			return next(
+				createHttpError.Conflict("User with that email already exists")
+			);
+		}
+		if (usernameExists) {
+			return next(
+				createHttpError.Conflict(
+					"User with that username already exists"
+				)
+			);
+		}
+
+		const hashedPassword = await argon2.hash(password);
+
 		const user = await prisma.user.create({
 			data: {
 				name,
 				email,
 				username,
-				password,
+				password: hashedPassword,
 			},
 		});
 
@@ -36,6 +71,21 @@ export const registerUser = async (
 	} catch (error) {
 		console.log(error);
 		logger.error(error);
+
+		if (error.isJoi === true) {
+			if (error.details[0].message.includes("email")) {
+				return next(createHttpError.BadRequest("Email is invalid"));
+			} else if (error.details[0].message.includes("username")) {
+				return next(createHttpError.BadRequest("Username is invalid"));
+			} else if (error.details[0].message.includes("password")) {
+				return next(
+					createHttpError.BadRequest(
+						"Password must be atleast 8 characters long."
+					)
+				);
+			}
+			return next(createHttpError.BadRequest(error.details[0].message));
+		}
 		return next(
 			createHttpError.InternalServerError("Unable to register user")
 		);
