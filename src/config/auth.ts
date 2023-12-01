6aa57152c8b701f0f09d67141ecaa91395d96f36
Vector-ops <sumith2827@gmail.com>
2023-12-01 23:01:21 +0530

    feat: add Google authentication and user registration

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -3,6 +3,7 @@ import argon2 from "argon2";
 import passport, { Profile } from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as LocalStrategy } from "passport-local";
+import { logger } from "../utils/logger";
 import prisma from "./prisma";
 
 const verifyLocal = async (
@@ -38,18 +39,39 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID || "",
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-			callbackURL: "/auth/google/callback",
+			callbackURL: "http://localhost:3000/api/auth/google/callback",
 		},
-		(
+		async (
 			_accessToken: string,
 			_refreshToken: string,
 			profile: Profile,
 			done: (error: any, user?: any) => void
 		) => {
-			// Handle user data or authentication here
-			// 'profile' contains user details returned by Google
-			console.log(profile);
-			return done(null, profile);
+			try {
+				const user = await prisma.user.findUnique({
+					where: {
+						email: profile.emails![0].value,
+					},
+				});
+				if (user) {
+					return done(null, user);
+				}
+
+				await prisma.user.create({
+					data: {
+						name: profile.displayName,
+						email: profile.emails![0].value,
+						username: profile.name!.givenName,
+						password: profile.id,
+						sub: profile.id,
+					},
+				});
+
+				return done(null, profile);
+			} catch (error) {
+				logger.error(error);
+				return done(error);
+			}
 		}
 	)
 );
@@ -69,6 +91,7 @@ passport.deserializeUser<any, any>(async (id, done) => {
 
 		done(null, user);
 	} catch (error) {
+		logger.error(error);
 		done(error);
 	}
 });
