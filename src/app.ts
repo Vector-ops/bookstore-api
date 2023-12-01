4ae6da27175b1be7daa510c2e8fb1ed9b2c8e608
Vector-ops <sumith2827@gmail.com>
2023-12-01 23:00:53 +0530

    build: configure cors

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,10 @@ import expressSession from "express-session";
 import passport from "passport";
 import prisma from "./config/prisma";
 import authRouter from "./routes/auth.routes";
-import { morganMiddleware } from "./utils/logger";
-require("./config/passport");
+import { booksRouter } from "./routes/books.routes";
+import { profileRouter } from "./routes/profile.routes";
+import { logger, morganMiddleware } from "./utils/logger";
+require("./config/auth");
 
 dotenv.config();
 
@@ -20,7 +22,18 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5173/",
+			"http://localhost:3000",
+			"http://localhost:3000/api/auth/login",
+			"http://localhost:5173",
+		],
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+	})
+);
 app.use(bodyParser.json());
 // app.use(morgan("dev"));
 app.use(morganMiddleware);
@@ -49,9 +62,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api/auth", authRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/books", booksRouter);
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
+	logger.info(`Server started on port ${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+	await prisma.$disconnect();
+	logger.info("SIGINT signal received. Stopping server.");
+	process.exit();
 });
